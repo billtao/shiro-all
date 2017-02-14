14b798021a70c3db1978c02b153e8a9ac9fc4d82
taotouhong <taotouhong@sinosoft.com.cn>
2017-02-14 17:45:51 +0800

    配置shiro,集成springmvc

--- shiro-all/src/main/java/j2se/HelloWorld.java
+++ shiro-all/src/main/java/j2se/HelloWorld.java
@@ -1,14 +1,11 @@
 package j2se;
 
-import javax.security.sasl.AuthenticationException;
-
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.util.Factory;
 
-import junit.framework.Assert;
 
 public class HelloWorld {
 
@@ -21,7 +18,7 @@ public class HelloWorld {
 	    SecurityUtils.setSecurityManager(securityManager);  
 	    //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）  
 	    Subject subject = SecurityUtils.getSubject();  
-	    UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+	    UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123456");
 	    
 	    try {  
 	        //4、登录，即身份验证  
